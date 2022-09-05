cb1bef0c42badfdef1e7ac1dc6f438beec2fc419
Daniel Caldas <daniel.caldasgoncalves@gmail.com>
2022-09-05 17:56:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuEffect(){
+    let nav = document.querySelector('.nav-items')
+    let burguerbar = document.querySelector('.menu-burguer')
+
+    burguerbar.classList.toggle('burguerEffect')
+    nav.classList.toggle('navbar')
+}
